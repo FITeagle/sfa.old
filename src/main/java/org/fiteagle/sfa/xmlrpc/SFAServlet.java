61e653239ffcc86950660f87297f59c72cebfcb2
dnehls <daniel.nehls@tu-berlin.de>
2014-02-14 15:24:16 +0100

    initial xmlrpc servlet commit

---
+++ src/main/java/org/fiteagle/sfa/xmlrpc/SFAServlet.java
@@ -0,0 +1,34 @@
+package org.fiteagle.sfa.xmlrpc;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import redstone.xmlrpc.XmlRpcServlet;
+
+public class SFAServlet extends XmlRpcServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8236792167953739154L;
+	
+
+	
+	public SFAServlet(){
+		
+	}
+	
+	
+	@Override
+	public void doPost(HttpServletRequest arg0, HttpServletResponse arg1)
+			throws ServletException, IOException {
+		
+		
+	}
+	
+	
+
+}
---
+++ src/test/java/org/fiteagle/sfa/xmlrpc/SFAServletTest.java
@@ -0,0 +1,44 @@
+package org.fiteagle.sfa.xmlrpc;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.easymock.EasyMock;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SFAServletTest {
+
+	SFAServlet servlet;
+	@Before
+	public void createTest(){
+		servlet = new SFAServlet();
+	}
+	@Test
+	public void test() {
+		assertNotNull(servlet);
+	}
+
+	
+	@Test
+	public void testDoPost() throws ServletException, IOException{
+		HttpServletRequest req = EasyMock.createMock(HttpServletRequest.class);
+		HttpServletResponse res = EasyMock.createMock(HttpServletResponse.class);
+		res.setCharacterEncoding(EasyMock.anyString());
+		EasyMock.expectLastCall().anyTimes();
+		res.setContentType(EasyMock.anyString());
+		EasyMock.expectLastCall().anyTimes();
+		ServletInputStream inputStream = EasyMock.createMock(ServletInputStream.class);
+		EasyMock.expect(req.getInputStream()).andReturn( inputStream);
+		EasyMock.replay(inputStream);
+		EasyMock.replay(req);
+		EasyMock.replay(res);
+		servlet.doPost(req, res);
+	}
+}
